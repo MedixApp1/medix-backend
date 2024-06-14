1ca8368c2bebcb0fa027623f82ea53ab82127843
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-06-14 23:57:07 +0100

    fixed

--- src/common/utils/google-speech.service.ts
+++ src/common/utils/google-speech.service.ts
@@ -1,8 +1,8 @@
 // google-speech.service.ts
+import { Socket } from 'socket.io';
 import { Injectable } from '@nestjs/common';
 import { SpeechClient } from '@google-cloud/speech';
 import * as apiKey from '../../medix-424107-59f23af2a167.json';
-import recoder from 'node-record-lpcm16';
 import { google } from '@google-cloud/speech/build/protos/protos';
 interface TranscriptItem {
   id: string;
@@ -21,92 +21,113 @@ export class GoogleSpeechService {
   constructor() {
     this.client = new SpeechClient({ apiKey });
   }
+}
 
-  async convertSpeechToText(
-    audioContent: Uint8Array,
-    // isFinal = false,
-    // languageCode = 'en-US',
-  ) {
-    // Creates a client
-    const client = this.client;
+@Injectable()
+export class SpeechService {
+  private speechClient: SpeechClient;
+  private recognizeStream: any = null;
+  private transcript: any;
+  private currentOffset: number;
 
-    /**
-     * TODO(developer): Uncomment the following lines before running the sample.
-     */
-    const encoding = 'LINEAR16';
-    const sampleRateHertz = 16000;
-    const languageCode = 'en-US';
+  constructor() {
+    this.speechClient = new SpeechClient({ apiKey });
+    this.transcript = null;
+    this.currentOffset = 0;
+  }
 
-    const request = {
+  startRecognitionStream(client: Socket, data: any) {
+    console.log('* StartRecognitionStream\n');
+    try {
+      this.recognizeStream = this.speechClient
+        .streamingRecognize(this.getRequest())
+        .on('error', console.error)
+        .on('data', (data) => {
+          console.log(data)
+          const result = data.results[0];
+          const isFinal = result.isFinal;
+
+          const transcription = data.results
+            .map((result) => result.alternatives[0].transcript)
+            .join('\n');
+
+          console.log(`Transcription: `, transcription);
+
+          this.transcript = {
+            id: `generated_id_${Math.random().toString(36).substring(2, 15)}`,
+            text: transcription,
+            start_offset_ms: this.currentOffset,
+            end_offset_ms:
+              this.currentOffset + result.resultEndTime.nanos * 1000,
+            is_final: isFinal,
+            object: 'transcript_item',
+          };
+
+          if (data.results[0] && data.results[0].isFinal) {
+            this.stopRecognitionStream();
+            this.startRecognitionStream(client, data);
+            console.log('restarted stream serverside');
+          }
+        })
+        .write(data.audio);
+      return this.transcript;
+    } catch (err) {
+      console.error('Error streaming google api ' + err);
+    }
+  }
+
+  stopRecognitionStream() {
+    if (this.recognizeStream) {
+      console.log('* StopRecognitionStream \n');
+      this.recognizeStream.end();
+    }
+    this.recognizeStream = null;
+  }
+
+  handleAudioData(audioData: { audio: Buffer }) {
+    if (this.recognizeStream !== null) {
+      try {
+        this.recognizeStream.write(audioData.audio);
+      } catch (err) {
+        console.log('Error calling google api ' + err);
+      }
+    } else {
+      console.log('RecognizeStream is null');
+    }
+  }
+
+  private getRequest() {
+    return {
       config: {
-        encoding: encoding,
-        sampleRateHertz: sampleRateHertz,
+        encoding: 'LINEAR16',
+        sampleRateHertz: 16000,
         languageCode: 'en-US',
-        //alternativeLanguageCodes: alternativeLanguageCodes,
         enableWordTimeOffsets: true,
         enableAutomaticPunctuation: true,
         enableWordConfidence: true,
         enableSpeakerDiarization: true,
-        //diarizationSpeakerCount: 2,
-        //model: "video",
         model: 'command_and_search',
-        //model: "default",
         useEnhanced: true,
       },
       interimResults: true,
-    };
-
-    // Create a recognize stream
-    const recognizeStream = client
-      .streamingRecognize(
-        request as google.cloud.speech.v1.IStreamingRecognitionConfig,
-      )
-      .on('error', console.error)
-      .on('data', (data) =>
-        process.stdout.write(
-          data.results[0] && data.results[0].alternatives[0]
-            ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-            : '\n\nReached transcription time limit, press Ctrl+C\n',
-        ),
-      );
-
-    // Start recording and send the microphone input to the Speech API.
-    // Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
-
-    // const transcript = {
-    //   id: `generated_id_${Math.random().toString(36).substring(2, 15)}`,
-    //   text: response.results
-    //     .map((result) => result.alternatives[0].transcript)
-    //     .join('\n'),
-    //   start_offset_ms: this.currentOffset,
-    //   end_offset_ms:
-    //     this.currentOffset + result.resultEndTime.nanos || 0 * 1000,
-    //   is_final: isFinal,
-    //   object: 'transcript_item',
-    // };
-
-    // transcripts.push(transcript);
-    // this.currentOffset = transcript.end_offset_ms;
-    // transcripts.push(result)
-
-    return result;
-  }
-
-  async convertSpeechToTextStream(
-    audioChunks: AsyncIterableIterator<Uint8Array>,
-  ): Promise<TranscriptItem[]> {
-    const transcripts: TranscriptItem[] = [];
-    let isFinal = false;
-
-    for await (const audioChunk of audioChunks) {
-      const chunkTranscripts = await this.convertSpeechToText(
-        audioChunk,
-        isFinal,
-      );
-      transcripts.push(...chunkTranscripts);
-      isFinal = true; // Set isFinal to true for subsequent chunks
-    }
-
-    return transcripts;
+    } as google.cloud.speech.v1.IStreamingRecognitionConfig;
   }
 }
+
+// const transcript = {
+//   id: `generated_id_${Math.random().toString(36).substring(2, 15)}`,
+//   text: response.results
+//     .map((result) => result.alternatives[0].transcript)
+//     .join('\n'),
+//   start_offset_ms: this.currentOffset,
+//   end_offset_ms:
+//     this.currentOffset + result.resultEndTime.nanos || 0 * 1000,
+//   is_final: isFinal,
+//   object: 'transcript_item',
+// };
+
+// // transcripts.push(transcript);
+// // this.currentOffset = transcript.end_offset_ms;
+// // transcripts.push(result)
+
+// return result;
