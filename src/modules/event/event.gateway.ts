bebcac3ac1d14d2de700ca5d98e85dd664e5a852
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-06-14 22:07:26 +0100

    updated gateway

--- src/modules/event/event.gateway.ts
+++ src/modules/event/event.gateway.ts
@@ -9,38 +9,43 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   SubscribeMessage,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { GoogleSpeechService } from 'src/common/utils/google-speech.service';
+import { OnModuleInit } from '@nestjs/common';
+import { IsBase64, isBase64 } from 'class-validator';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
-  }
+  },
 })
 // @Injectable()
-export class EventGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class EventGateway implements OnModuleInit {
+  @WebSocketServer()
   server: Server;
 
   constructor(private readonly googleSpeechService: GoogleSpeechService) {
     this.server = new Server({ noServer: true });
   }
-  handleConnection(client: Socket) {
-    console.log('Client connected');
-  }
-
-  async handleDisconnect(client: Socket) {
-    console.log('Client disconnected');
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(`Connected with socket id: ${socket.url}`);
+    });
   }
 
   @SubscribeMessage('speech-to-text')
-  async handleMessage(client: Socket, data: any) {
-    console.log(data)
-    if (data instanceof ArrayBuffer) {
-      const audioData = new Uint8Array(data);
+  async handleMessage(client: Socket, data: string) {
+    const stuff = JSON.parse(data);
+    const audio = stuff.payload;
+    if (audio) {
+      const audioData = Uint8Array.from(Buffer.from(audio, 'base64'));
+      // const audioData = new Uint8Array(audio);
       const audioStream = this.createAsyncIterable(audioData);
       try {
         const text =
           await this.googleSpeechService.convertSpeechToTextStream(audioStream);
+        console.log(text);
         client.send(text);
       } catch (error) {
         console.error('Error during speech recognition:', error);
