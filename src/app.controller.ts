de6bc33818e3ba11254d18382714c41c21e56d91
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-07-01 06:01:53 +0100

    fixed issue with shege

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,15 +14,5 @@ import { FileInterceptor } from '@nestjs/platform-express';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
-  @Post('/speech-to-text')
-  @UseInterceptors(FileInterceptor('file'))
-  async speechToText(@UploadedFile() file: Express.Multer.File) {
-    const blobFile = new Blob([file.buffer]);
-    return await audioToText(blobFile);
-  }
 }
